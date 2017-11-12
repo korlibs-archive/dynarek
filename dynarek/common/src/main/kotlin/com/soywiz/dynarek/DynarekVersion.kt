e2b6c62e4857a39f4d28f5e2f09bc74615232fe0
soywiz <soywiz@gmail.com>
2017-11-12 23:33:35 +0100

    First commit

---
+++ dynarek/common/src/main/kotlin/com/soywiz/dynarek/DynarekVersion.kt
@@ -0,0 +1,3 @@
+package com.soywiz.dynarek
+
+val DYNAREK_VERSION = "0.0.1-SNAPSHOT"
---
+++ dynarek/common/src/test/kotlin/com/soywiz/dynarek/DrekTest.kt
@@ -0,0 +1,35 @@
+package com.soywiz.dynarek
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DrekTest {
+	@Test
+	fun testInterpreter() {
+		val function = function(DClass(State::class), DINT, DVOID) {
+			SET(p0[State::a], p1 * 2.lit)
+		}
+
+		val state = State()
+		val interpreter = DSlowInterpreter(listOf(state, 10))
+		interpreter.interpret(function)
+		assertEquals(20, state.a)
+	}
+
+	@Test
+	fun testDynarek() {
+		val function = function(DClass(State::class), DINT, DVOID) {
+			//IF(true) {
+			SET(p0[State::a], p0[State::a] + 4 * p1)
+			//} ELSE {
+			//	SET(p0[State::b], 4 * p1)
+			//}
+		}
+		val state = State(a = 7)
+		val func = function.generateDynarek()
+		val ret = func(state, 2)
+
+		assertEquals(15, state.a)
+	}
+}
+
